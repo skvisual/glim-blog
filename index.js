1b55d47c6263258ab4dc41750ba0d27650f2c88b
skvisual <southkvisual@gmail.com>
2020-05-21 13:03:52 -0500

    added middleware to handle images and form validation

--- index.js
+++ index.js
@@ -4,14 +4,27 @@ const ejs = require('ejs');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const BlogPost = require('./models/BlogPost');
+const fileUpload = require('express-fileupload');
 
 const app = new express();
 const PORT = 3000;
 
+const validateMiddleWare = (req,res,next) => {
+    if(req.files == null || req.body.title == null || req.body.title == null) {
+        return res.redirect('/posts/new')
+    }
+    next()
+}
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+app.use('/posts/store',validateMiddleWare)
 app.set('view engine', 'ejs');
+
+
+
 
 // HTML ROUTES
 
@@ -43,10 +56,17 @@ app.get('/posts/new', (req, res)=> {
     res.render('create')
 })
 
-app.post('/posts/store', async (req, res) => {
-    await BlogPost.create(req.body)
-    res.redirect('/')
+app.post('/posts/store', (req, res) => {
+    let image = req.files.image;
+    image.mv(path.resolve(__dirname, 'public/img', image.name), async (err) => {
+        await BlogPost.create({
+            ...req.body,
+            image:'/img/' + image.name
+        })
+        res.redirect('/')
+        })
 })
+
 
 mongoose.connect('mongodb://localhost/blog_db', {useNewUrlParser: true})
 
--- models/BlogPost.js
+++ models/BlogPost.js
@@ -8,7 +8,8 @@ const BlogPostSchema = new Schema({
     datePosted: {
         type: Date,
         default: new Date()
-    }
+    },
+    image: String
 });
 
 const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
